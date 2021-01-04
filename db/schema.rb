eb2f0e7a4ce90181e440493dfd02383ed71b2a77
Al Fian <al.fian.v001@gmail.com>
2021-01-04 13:13:23 +0700

    Added Picture model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_055847) do
+ActiveRecord::Schema.define(version: 2021_01_04_061000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_01_04_055847) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id", "friend_id"], name: "index_bonds_on_user_id_and_friend_id", unique: true
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.string "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -49,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_055847) do
 
   add_foreign_key "bonds", "users"
   add_foreign_key "bonds", "users", column: "friend_id"
+  add_foreign_key "pictures", "posts"
   add_foreign_key "posts", "posts", column: "thread_id"
   add_foreign_key "posts", "users"
 end
