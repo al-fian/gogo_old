7c011794788958768ca65103dd5de0429550cbb5
Al Fian <al.fian.v001@gmail.com>
2021-01-04 12:44:21 +0700

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_04_053748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "first_name", null: false
+    t.string "last_name"
+    t.string "email", null: false
+    t.boolean "is_public", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
 end
