4550566e5830e890f760fd5d99172cf1807e8cbb
Al Fian <al.fian.v001@gmail.com>
2021-01-05 09:43:45 +0700

    Passed user's email tests for any bogus emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
 
   validates :first_name, presence: true
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
