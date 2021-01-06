f350e4df565640e2402f289eb84025909dc02221
Al Fian <al.fian.v001@gmail.com>
2021-01-06 08:11:57 +0700

    Added the 'Reset Password' page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Required by letter_opener_web gem
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
