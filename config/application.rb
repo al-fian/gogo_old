8254e9c5f609f794f6c0d96a2bbfb950b93cfcb1
Al Fian <al.fian.v001@gmail.com>
2021-01-06 10:02:39 +0700

    Added style to Mailer Emailer Layout

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module Gogo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.to_prepare do
+      Devise::Mailer.layout "mailer"
+    end
   end
 end
