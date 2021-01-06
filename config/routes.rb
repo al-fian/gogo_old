bc3675c2b5edf00d36fcc67c55c50213589d3013
Al Fian <al.fian.v001@gmail.com>
2021-01-06 10:31:14 +0700

    Created a partial '_header'

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'timelines/index'
-  get 'timelines/show'
   devise_for :users
   root to: 'home#index'
   get 'home/index'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  authenticate :user do
+    resources :timelines,
+             only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
