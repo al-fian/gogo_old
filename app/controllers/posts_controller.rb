cbe737edbe22fb52a019a14ec75355451a1b0c68
Al Fian <al.fian.v001@gmail.com>
2021-01-08 11:32:55 +0700

    Built posting form for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
   def create
   end
+
+  private
+
+  def permitted_params
+    params.require(:post).permit(
+      :postable_type,
+      :status_text
+    )
+  end
 end
