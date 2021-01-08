45d66b746178c3602dce59901431844d3576c811
Al Fian <al.fian.v001@gmail.com>
2021-01-08 09:23:34 +0700

    Added RSpec test on user_decorator_spec.rb

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -3,7 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
-  let(:user) { User.new.extend UserDecorator }
+  let(:user) { create(:user).extend UserDecorator }
   subject { user }
-  it { should be_a User }
+  describe "#name" do
+    it "returns a full name" do
+      expect(user.name).to eq "#{user.first_name} #{user.last_name}"
+    end
+  end
+
+  describe "#profile_picture_url" do
+    it "returns a gravatar URL" do
+      expect(user.profile_picture_url).to \
+        start_with("https://www.gravatar.com")
+    end
+  end
 end
