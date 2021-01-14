6e3d2493160ed829e18aef5890a075a088e614c3
Al Fian <al.fian.v001@gmail.com>
2021-01-14 13:20:33 +0700

    Modified settings.css as settings1.css

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -1,16 +1,20 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    variants: {},
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            colors: {
+                main: {
+                    default: "#60b0e2",
+                    "200": "#6cc1ff",
+                    "500": "#41a1dd",
+                    "800": "#04609a",
+                },
+                dark: {
+                    "400": "#8a8a8a",
+                    "500": "#4f4f4f",
+                    "600": "#343538",
+                    "700": "#2a2b2d",
+                    "800": "#18191a"
+                }
             },
             spacing: {
                 '14': '3.5rem',
@@ -19,21 +23,9 @@ module.exports = {
                 '96': '24rem',
                 '168': '42rem',
                 '192': '48rem',
-            },
-            colors: {
-              main: {
-                  default: "#60b0e2",
-                  "200": "#6cc1ff",
-                  "500": "#41a1dd",
-                  "800": "#04609a",
-              },
-               dark: {
-                  "400": "#8a8a8a",
-                  "600": "#343538",
-                  "700": "#2a2b2d",
-                  "800": "#18191a"
-               }
-            },
+            }
         },
-    }
+    },
+    variants: {},
+    plugins: [],
 }
