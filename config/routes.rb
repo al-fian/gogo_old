34b8298cbbd5103b996c2342344d6eb668586940
Al Fian <al.fian.v001@gmail.com>
2021-01-18 11:45:52 +0700

    Solved problem with 'bin/webpack' did not run

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  get 'home/index'
+  # get 'home/index'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
